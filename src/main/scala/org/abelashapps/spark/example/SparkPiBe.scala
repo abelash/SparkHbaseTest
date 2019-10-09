981ba8cae557cd8b9c560dfc6367aa5e5ad7b7f4
Alex Belash <abelash@gmail.com>
2019-10-09 13:42:45 +0300

    Added SparkPiBe  simple example for Kubernetes cluster

---
+++ src/main/scala/org/abelashapps/spark/example/SparkPiBe.scala
@@ -0,0 +1,34 @@
+package org.abelashapps.spark.example
+
+// scalastyle:off println
+
+import org.apache.spark.sql.SparkSession
+
+import scala.math.random
+
+/** Computes an approximation to pi */
+object SparkPiBe {
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .appName("Spark Pi")
+      .config("spark.master", "local[8]")
+      .getOrCreate()
+
+    val slices = if (args.length > 0) args(0).toInt else 2
+    val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
+    val count = spark.sparkContext.parallelize(1 until n, slices).map { i =>
+      val x = random * 2 - 1
+      val y = random * 2 - 1
+      if (x*x + y*y <= 1) 1 else 0
+    }.reduce(_ + _)
+    println(s"Pi is roughly ${4.0 * count / (n - 1)}")
+    println("Helloooooooooooooooooooooooooooooooo Kubernetes Operator!")
+    println("Hellooooooooooooooooooooooooooooooooo Kubernetes Operator!")
+    println("Helloooooooooooooooooooooooooooooooooo Kubernetes Operator!")
+    Thread.sleep(600000)
+    spark.stop()
+  }
+}
+// scalastyle:on println
+
