11710d94ef27ce84bc69828c4c4bb6d9da0d736c
Alex Belash <abelash@gmail.com>
2019-10-15 10:05:17 +0300

    Review simple test and found that some configuration doesn't work as expected

--- src/main/scala/org/abelashapps/spark/example/SimpleHbaseTest.scala
+++ src/main/scala/org/abelashapps/spark/example/SimpleHbaseTest.scala
@@ -3,19 +3,19 @@ package org.abelashapps.spark.example
 import org.apache.hadoop.hbase.{HBaseConfiguration, MasterNotRunningException}
 import org.apache.hadoop.hbase.client.HBaseAdmin
 
-object SimpleHbaseTest {
+object SimpleHbaseTest extends App {
   val config = HBaseConfiguration.create()
   var hbaseRunning = false;
 
   config.clear
-  config.set("hbase.zookeeper.quorum", "172.18.0.2");  // This ip Address was retrieved by doing a docker inspect on the docker container
+  config.set("hbase.zookeeper.quorum", "127.0.0.1");  // This ip Address was retrieved by doing a docker inspect on the docker container
   config.set("hbase.zookeeper.property.clientPort","2181")
-  config.set("hbase.master", "172.18.0.2:60000");
+  //DOES'NOT  INFLUENCE ON TEST RESULT config.set("hbase.master", "127.0.0.1:16000");
   config.setInt("timeout", 25000);
 
   try{
     println(" Check if HBase is Running")
-    //HBaseAdmin.checkHBaseAvailable(config);   // This line throws the exception
+    HBaseAdmin.checkHBaseAvailable(config);   // This line throws the exception
     println(" HBase is Running")              // Because this line doesnt print.
     hbaseRunning = true
   }catch{
@@ -23,6 +23,6 @@ object SimpleHbaseTest {
       println(" MasterNotRunningException |" + ex.getMessage)  // This is the exception I get
     }
   }
-  finally println(" HBase is NOT Running")
+  finally println(" HBase test finalized!")
 
 }
